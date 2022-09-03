76961d4bf6ed1a6ffd6622c0ca3a086e595edf60
RotenKiwi <joshiparth212@gmail.com>
2022-09-03 12:11:29 +0530

    initialised login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'LoginScreen.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false;
+    debugShowCheckedModeBanner: false,
     home: LoginScreen(),
     ); //MaterialApp
   } //Widget
