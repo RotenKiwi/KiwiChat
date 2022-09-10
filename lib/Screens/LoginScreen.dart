bbd2e0b7fb0cad28949c6f60c1340908df09060f
RotenKiwi <joshiparth212@gmail.com>
2022-09-10 19:00:07 +0530

    added back button on loginscreen

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kiwichat/Screens/WelcomeScreen.dart';
+import 'package:kiwichat/components/RoundedButton.dart';
 import 'package:kiwichat/components/background.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -6,24 +8,35 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Background(
-        child: Stack(children: const <Widget>[
+        child: Stack(children: <Widget>[
       Positioned(
+        top: 20,
+        left: 20,
+        child: RoundedButton(
+          text: 'Back',
+          press: () {Navigator.push(context, MaterialPageRoute(builder: (context){return const WelcomeScreen();},),);},
+          color: const Color(0xff295e13),
+          textColor: Colors.white,
+          length: size * 0.2,
+        ),
+      ),
+      const Positioned(
         top: 200,
-        right: 45,
+        right: 35,
         child: Text(
           'Kiwi \nChat',
           style: TextStyle(
-            
             fontWeight: FontWeight.w900,
             fontSize: 55,
             color: Color(0xff295e13),
           ),
         ),
       ),
-      Positioned(
+      const Positioned(
           top: 200,
-          left: 45,
+          left: 55,
           child: Image(image: AssetImage('assets/images/logo.png'))),
     ]));
   }
