1eb19efe7c441628b3eec6ff9f2c9bb3e38add04
RotenKiwi <joshiparth212@gmail.com>
2022-09-14 23:53:43 +0530

    unnecesary changes

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -51,7 +51,7 @@ class LoginScreen extends StatelessWidget {
       ),
       //name 'kiwichat'
       const Positioned(
-          top: 200,
+          top: 210,
           left: 65,
           child: Image(image: AssetImage('assets/images/logo.png'))),
       //logo
